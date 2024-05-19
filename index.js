063053f670f000c40af08780e3e202e26eead447
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-05-20 02:09:44 +0600

    intial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("bistro boss is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
